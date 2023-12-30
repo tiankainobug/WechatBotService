db5cea72390cebf9009ad3f8054ac1c049d71995
tiankai <tiankai@chinatelecom.cn>
2023-12-30 18:06:23 +0800

    fix: 成功后发送回执

--- utils/schedule-job.js
+++ utils/schedule-job.js
@@ -9,6 +9,12 @@ async function start(bot, data) {
             const weiba = await bot.Room.find({topic: data.topic})
             weiba.say(fileBox)
             weiba.say(dataItem.text)
+
+            // 发送成功后，通过文件传输助手，给自己一个回执。
+            const success = await bot.Room.find({topic: '文件传输助手'})
+            success.say("主人，定时任务已成功发送！消息如下：")
+            success.say(fileBox)
+            success.say(dataItem.text)
         } catch (err) {
             console.log('错误：\n', err)
         }
